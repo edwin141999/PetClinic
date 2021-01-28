b63aae42c2b8a8c01c9542904b6abdfe4e05e6ea
EduDev011098 <183423@ids.upchiapas.edu.mx>
2021-01-28 17:13:43 -0600

    about con css

---
+++ src/main/java/org/springframework/samples/petclinic/about/AboutController.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.about;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String about() {
+
+        return "about";
+    }
+
+}
